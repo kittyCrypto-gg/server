ed25ed075b2584ddf6b3587b1a837fecbb3244d1
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-07-27 20:21:15 +0100

    Expose a stable server package API
    * Add stable server package exports
    
    * Expose a stable package API
    
    * Test the public server package API

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { default as Server } from "./baseServer.ts";
+export {
+  MutexProtoBuffStore,
+  ProtoBuffObjectCodec,
+  type MutexProtoBuffStoreOptions,
+  type ProtoBuffCodec,
+  type ProtoBuffObjectCodecOptions
+} from "./mutexPBstore.ts";
